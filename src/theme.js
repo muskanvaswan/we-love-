c38e5831c5c732d1c9299052706fc5635790ef80
Muskan Vaswan <muskanvaswan@gmail.com>
2023-04-01 19:25:29 +0530

    reverting to round 1

--- src/theme.js
+++ src/theme.js
@@ -3,13 +3,12 @@ import { red } from '@mui/material/colors';
 
 // Create a theme instance.
 const theme = createTheme({
-  mode: "light",
   palette: {
     primary: {
       main: '#556cd6',
     },
     secondary: {
-      main: '#D5d5d5',
+      main: '#19857b',
     },
     error: {
       main: red.A400,
